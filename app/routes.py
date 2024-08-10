56145998de45b07ad663493095ef0e5bfa97ead8
TrolljanO <7r4j4n0@gmail.com>
2024-08-09 22:03:41 -0300

    falta resolver o bug de upload e gerar pix

--- app/routes.py
+++ app/routes.py
@@ -14,108 +14,19 @@ import boto3
 
 load_dotenv()
 
-# Criação de um Blueprint para as rotas principais
 main = Blueprint('main', __name__)
 
-# Configurar o nome do bucket S3 e o caminho permitido para uploads
 BUCKET_NAME = 'l769ab'
 ALLOWED_EXTENSIONS = {'zip', 'rar', 'RAR', 'ZIP'}
 
-
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-
 
 @main.route('/')
 @login_required
 def index():
     files_in_process = FileLog.query.filter_by(user_id=current_user.id).order_by(FileLog.upload_date.desc()).all()
-    files_ready = FileLog.query.filter_by(user_id=current_user.id, status='Pronto').all()
-
-    return render_template('index.html',
-                           files_in_process=files_in_process,
-                           files_ready=files_ready,
-                           credits=current_user.credits)
-
-
-@main.route('/upload_file', methods=['POST'])
-@login_required
-def upload_file():
-    if 'file' not in request.files:
-        return jsonify({'error': 'Nenhum arquivo foi enviado.'}), 400
-
-    file = request.files['file']
-
-    if file.filename == '':
-        return jsonify({'error': 'Nenhum arquivo foi selecionado.'}), 400
-
-    if file and allowed_file(file.filename):
-        filename = secure_filename(file.filename)
-        try:
-            # Calcular o número de pastas antes do upload
-            with zipfile.ZipFile(BytesIO(file.read()), 'r') as zip_ref:
-                num_folders = sum(1 for z in zip_ref.namelist() if z.endswith('/'))
-            cost = num_folders * 1.00  # Exemplo de cálculo: R$1,00 por pasta
-
-            # Fazer o upload para o S3
-            file.seek(0)  # Reposicionar para o início antes do upload
-            s3_key = f"{current_user.id}/{filename}"
-            s3.upload_fileobj(
-                file,
-                BUCKET_NAME,
-                s3_key,
-                ExtraArgs={"ContentType": file.content_type}
-            )
-
-            # Registrar no banco de dados
-            new_log = FileLog(user_id=current_user.id, filename=filename, s3_key=s3_key,
-                              status='Aguardando Autorização', cost=cost)
-            db.session.add(new_log)
-            db.session.commit()
-
-            return jsonify({'custo': cost, 'num_folders': num_folders, 'file_id': new_log.id})
-
-        except Exception as e:
-            app.logger.error(f'Ocorreu um erro ao fazer o upload: {str(e)}')
-            return jsonify({'error': f'Ocorreu um erro ao fazer o upload: {str(e)}'}), 500
-
-    return render_template('index.html')
-
-
-@main.route('/process_authorization', methods=['POST'])
-@login_required
-def process_authorization():
-    file_id = request.form.get('file_id')
-    file_log = FileLog.query.get(file_id)
-    if file_log:
-        user = current_user
-        if user.credits >= file_log.cost:
-            user.credits -= file_log.cost
-            file_log.status = 'Em Processamento'
-            db.session.commit()
-            return jsonify({'success': True})
-        else:
-            return jsonify({'success': False, 'message': 'Saldo insuficiente'}), 400
-    return jsonify({'success': False, 'message': 'Arquivo não encontrado'}), 404
-
-
-@main.route('/cancel_process', methods=['POST'])
-@login_required
-def cancel_process():
-    file_id = request.form['file_id']
-    file_log = FileLog.query.get(file_id)
-    file_log.status = 'Cancelado'
-    db.session.commit()
-    flash('Processo cancelado.', 'warning')
-
-    return redirect(url_for('main.index'))
-
-
-@main.route('/profile')
-@login_required
-def profile():
-    return render_template('profile.html')
-
+    return render_template('index.html', files_in_process=files_in_process, credits=current_user.credits)
 
 @main.route('/finance', methods=['GET', 'POST'])
 @login_required
@@ -124,7 +35,9 @@ def finance():
         amount = request.form.get('amount')
         user = current_user
 
-        amount = amount.replace(',', '').replace('.', '')
+        # Certifique-se de que o valor está correto antes de enviar à API
+        if not amount.isdigit():
+            return jsonify({'error': 'Valor inválido. Insira um número inteiro.'}), 400
 
         api_key = os.getenv('OPENPIX_API_KEY')
         headers = {
@@ -173,6 +86,126 @@ def finance():
     transactions = Transaction.query.filter_by(user_id=current_user.id).order_by(Transaction.timestamp.desc()).all()
     return render_template('finance.html', transactions=transactions)
 
+@main.route('/upload_file', methods=['POST'])
+@login_required
+def upload_file():
+    if 'file' not in request.files:
+        return jsonify({'error': 'Nenhum arquivo foi enviado.'}), 400
+
+    file = request.files['file']
+
+    if file.filename == '':
+        return jsonify({'error': 'Nenhum arquivo foi selecionado.'}), 400
+
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        try:
+            with zipfile.ZipFile(BytesIO(file.read()), 'r') as zip_ref:
+                num_folders = sum(1 for z in zip_ref.namelist() if z.endswith('/'))
+            cost = num_folders * 1.00
+
+            user = current_user
+
+            if user.credits >= cost:
+                user.credits -= cost
+                db.session.commit()
+
+                file.seek(0)
+                s3_key = f"{current_user.id}/{filename}"
+                s3.upload_fileobj(
+                    file,
+                    BUCKET_NAME,
+                    s3_key,
+                    ExtraArgs={"ContentType": file.content_type}
+                )
+
+                new_log = FileLog(user_id=current_user.id, filename=filename, s3_key=s3_key,
+                                  status='Em Processamento', cost=cost)
+                db.session.add(new_log)
+                db.session.commit()
+
+                return jsonify(
+                    {'success': True, 'message': 'Processamento autorizado e iniciado.', 'file_id': new_log.id})
+            else:
+                new_log = FileLog(user_id=current_user.id, filename=filename, s3_key='',
+                                  status='Aguardando Pagamento', cost=cost)
+                db.session.add(new_log)
+                db.session.commit()
+
+                return jsonify({'success': False, 'message': 'Saldo insuficiente. Por favor, gere um PIX para continuar.', 'file_id': new_log.id, 'custo': cost})
+
+        except Exception as e:
+            app.logger.error(f'Ocorreu um erro ao fazer o upload: {str(e)}')
+            return jsonify({'error': f'Ocorreu um erro ao fazer o upload: {str(e)}'}), 500
+
+    return jsonify({'error': 'Tipo de arquivo não permitido.'}), 400
+
+@main.route('/process_authorization', methods=['POST'])
+@login_required
+def process_authorization():
+    file_id = request.form.get('file_id')
+    file_log = FileLog.query.get(file_id)
+    if file_log:
+        user = current_user
+        if user.credits >= file_log.cost:
+            user.credits -= file_log.cost
+            file_log.status = 'Em Processamento'
+            db.session.commit()
+            return jsonify({'success': True})
+        else:
+            return jsonify({'success': False, 'message': 'Saldo insuficiente'}), 400
+    return jsonify({'success': False, 'message': 'Arquivo não encontrado'}), 404
+
+@main.route('/cancel_process', methods=['POST'])
+@login_required
+def cancel_process():
+    file_id = request.form['file_id']
+    file_log = FileLog.query.get(file_id)
+    file_log.status = 'Cancelado'
+    db.session.commit()
+    flash('Processo cancelado.', 'warning')
+
+    return redirect(url_for('main.index'))
+
+@main.route('/generate_pix/<int:file_id>', methods=['POST'])
+@login_required
+def generate_pix(file_id):
+    file_log = FileLog.query.get(file_id)
+    if not file_log or file_log.status != 'Aguardando Pagamento':
+        return jsonify({'error': 'Arquivo não encontrado ou pagamento não necessário.'}), 400
+
+    api_key = os.getenv('OPENPIX_API_KEY')
+    headers = {
+        'Authorization': f'{api_key}',
+        'Content-Type': 'application/json'
+    }
+    payload = {
+        "name": f"Pagamento para {current_user.username}",
+        "correlationID": str(uuid.uuid4()),
+        "value": int(file_log.cost * 100),  # Valor em centavos
+        "comment": f"Pagamento do arquivo {file_log.filename}"
+    }
+
+    try:
+        response = requests.post('https://api.openpix.com.br/api/v1/qrcode-static', json=payload, headers=headers)
+        response.raise_for_status()
+
+        charge = response.json()
+        if 'pixQrCode' in charge:
+            qr_code = charge['pixQrCode']['qrCodeImage']
+            return jsonify({'qr_code': qr_code})
+        else:
+            return jsonify({'error': 'Erro ao gerar cobrança. Verifique a resposta da API.'}), 500
+
+    except requests.exceptions.HTTPError as http_err:
+        return jsonify({'error': f'HTTP error occurred: {http_err}'}), 500
+    except Exception as err:
+        return jsonify({'error': f'Other error occurred: {err}'}), 500
+
+@main.route('/profile')
+@login_required
+def profile():
+    return render_template('profile.html')
 
 @main.route('/register_webhook', methods=['POST'])
 @login_required
@@ -196,54 +229,18 @@ def register_webhook():
         return jsonify({'message': 'Webhook registrado com sucesso!', 'webhook_info': webhook_info})
 
     except requests.exceptions.HTTPError as http_err:
-        app.logger.error(f"HTTP error occurred: {http_err}")
-        return jsonify({'error': 'Erro ao registrar webhook. Verifique a API Key e os parâmetros.'}), 500
+        return jsonify({'error': f'HTTP error occurred: {http_err}'}), 500
     except Exception as err:
-        app.logger.error(f"Other error occurred: {err}")
-        return jsonify({'error': 'Erro ao registrar webhook. Verifique a API Key e os parâmetros.'}), 500
-
+        return jsonify({'error': f'Other error occurred: {err}'}), 500
 
 @main.route('/webhook', methods=['POST'])
 def handle_webhook():
     data = request.json
-    app.logger.info(f"Webhook recebido: {data}")
-
-    # Processar o webhook recebido e atualizar as transações conforme necessário
     correlation_id = data.get("pixQrCode", {}).get("correlationID")
     if correlation_id:
         transaction = Transaction.query.filter_by(correlation_id=correlation_id).first()
         if transaction:
             transaction.status = 'Concluído'
             db.session.commit()
-            app.logger.info(f"Transação {transaction.id} atualizada para 'Concluído'.")
 
     return jsonify({'status': 'success'}), 200
-
-
-def calcular_numero_de_pastas(s3_key):
-    s3 = boto3.client('s3')
-    BUCKET_NAME = 'l769ab'
-
-    s3_object = s3.get_object(Bucket=BUCKET_NAME, Key=s3_key)
-    file_content = s3_object['Body'].read()
-
-    with zipfile.ZipFile(BytesIO(file_content), 'r') as zip_ref:
-        num_pastas = sum(1 for z in zip_ref.namelist() if z.endswith('/'))
-
-    return num_pastas
-
-
-@main.route('/calculate_cost', methods=['POST'])
-@login_required
-def calculate_cost():
-    file_id = request.form['file_id']
-    s3_key = request.form['s3_key']
-
-    num_pastas = calcular_numero_de_pastas(s3_key)
-    custo = num_pastas * 1.00  # Exemplo: R$ 1,00 por pasta
-
-    file_log = FileLog.query.get(file_id)
-    file_log.cost = custo
-    db.session.commit()
-
-    return jsonify({'custo': custo})
--- app/static/js/scripts.js
+++ app/static/js/scripts.js
@@ -1,151 +1,49 @@
-// ---------Responsive-navbar-active-animation-----------
-function test() {
-    var tabsNewAnim = $('#navbarNav');
-    var selectorNewAnim = $('#navbarNav').find('li').length;
-    var activeItemNewAnim = tabsNewAnim.find('.active');
-    var activeWidthNewAnimHeight = activeItemNewAnim.innerHeight();
-    var activeWidthNewAnimWidth = activeItemNewAnim.innerWidth();
-    var itemPosNewAnimTop = activeItemNewAnim.position();
-    var itemPosNewAnimLeft = activeItemNewAnim.position();
-    $(".hori-selector").css({
-        "top": itemPosNewAnimTop.top + "px",
-        "left": itemPosNewAnimLeft.left + "px",
-        "height": activeWidthNewAnimHeight + "px",
-        "width": activeWidthNewAnimWidth + "px"
-    });
-    $("#navbarNav").on("click", "li", function () {
-        $('#navbarNav ul li').removeClass("active");
-        $(this).addClass('active');
-        var activeWidthNewAnimHeight = $(this).innerHeight();
-        var activeWidthNewAnimWidth = $(this).innerWidth();
-        var itemPosNewAnimTop = $(this).position();
-        var itemPosNewAnimLeft = $(this).position();
-        $(".hori-selector").css({
-            "top": itemPosNewAnimTop.top + "px",
-            "left": itemPosNewAnimLeft.left + "px",
-            "height": activeWidthNewAnimHeight + "px",
-            "width": activeWidthNewAnimWidth + "px"
-        });
-    });
-}
-
 $(document).ready(function () {
+    // Navbar animation
     setTimeout(function () { test(); });
-});
 
-$(window).on('resize', function () {
-    setTimeout(function () { test(); }, 500);
-});
-
-$(".navbar-toggler").click(function () {
-    $(".navbar-collapse").slideToggle(300);
-    setTimeout(function () { test(); });
-});
-
-// --------------add active class-on another-page move----------
-jQuery(document).ready(function ($) {
-    var path = window.location.pathname.split("/").pop();
-    if (path == '') path = 'index.html';
-    var target = $('#navbarNav ul li a[href="' + path + '"]');
-    target.parent().addClass('active');
-});
-
-// Função de upload de arquivo
-const uploadForm = document.querySelector('#uploadForm');
-const progressBar = document.querySelector('.progress-bar');
-
-if (uploadForm) {
-    uploadForm.addEventListener('submit', function (e) {
-        e.preventDefault();
-        const formData = new FormData(uploadForm);
-        const xhr = new XMLHttpRequest();
-
-        xhr.open('POST', uploadForm.action, true);
-
-        xhr.onload = function () {
-            if (xhr.status === 200) {
-                try {
-                    const response = JSON.parse(xhr.responseText);
-                    const custo = response.custo;
-                    const fileId = response.file_id;
-
-                    if (confirm(`Valor do processamento: R$ ${custo}. Deseja continuar?`)) {
-                        authorizeProcessing(fileId, custo);
-                    } else {
-                        cancelProcessing(fileId);
-                    }
-                } catch (e) {
-                    alert('Erro ao processar a resposta do servidor.');
-                    console.error('Erro ao processar JSON:', e);
-                }
-            } else {
-                alert('Erro no upload.');
-            }
-        };
-
-        xhr.upload.onprogress = function (event) {
-            if (event.lengthComputable) {
-                const percentComplete = (event.loaded / event.total) * 100;
-                progressBar.style.width = percentComplete + '%';
-                progressBar.setAttribute('aria-valuenow', percentComplete);
-                progressBar.textContent = Math.floor(percentComplete) + '%';
-
-                if (percentComplete === 100) {
-                    setTimeout(() => {
-                        progressBar.classList.add('bg-info');
-                        progressBar.textContent = 'Processando...';
-                    }, 500);
-                }
-            }
-        };
-
-        xhr.send(formData);
+    $(window).on('resize', function () {
+        setTimeout(function () { test(); }, 500);
     });
-}
 
-// Função de recarga via PIX
-const rechargeForm = document.querySelector('#recharge-form');
-const amountInput = document.getElementById('amount');
+    $(".navbar-toggler").click(function () {
+        $(".navbar-collapse").slideToggle(300);
+        setTimeout(function () { test(); });
+    });
 
-$(function() {
+    // Configuração do MaskMoney
     $('#amount').maskMoney({
         prefix: 'R$ ',
         allowNegative: false,
         thousands: '.',
         decimal: ',',
         affixesStay: true,
-        precision: 2 // Deixa a precisão em 2 casas decimais
+        precision: 2
     });
 
+    // Formulário de recarga via PIX
     $('#recharge-form').on('submit', function(e) {
         e.preventDefault();
 
-        // Pega o valor desformatado como número float
         let rawValue = $('#amount').maskMoney('unmasked')[0];
-
-        // Converte o valor para centavos
         let amountInCents = parseInt(rawValue * 100);
 
         const formData = new FormData();
-        formData.append('amount', amountInCents); // Envia o valor em centavos
+        formData.append('amount', amountInCents);
 
         const xhr = new XMLHttpRequest();
         xhr.open('POST', $(this).attr('action'), true);
 
         xhr.onload = function () {
             if (xhr.status === 200) {
-                try {
-                    const response = JSON.parse(xhr.responseText);
-                    if (response.qr_code) {
-                        const qrCodeImg = document.getElementById('qr-code');
-                        qrCodeImg.src = response.qr_code;
-                        qrCodeImg.style.display = 'block';
-                        document.getElementById('payment-status').textContent = 'Aguardando pagamento...';
-                    } else if (response.error) {
-                        alert(response.error);
-                    }
-                } catch (e) {
-                    alert('Erro ao processar a resposta do servidor.');
+                const response = JSON.parse(xhr.responseText);
+                if (response.qr_code) {
+                    const qrCodeImg = document.getElementById('qr-code');
+                    qrCodeImg.src = response.qr_code;
+                    qrCodeImg.style.display = 'block';
+                    document.getElementById('payment-status').textContent = 'Aguardando pagamento...';
+                } else if (response.error) {
+                    alert(response.error);
                 }
             } else {
                 alert('Erro ao gerar o código PIX.');
@@ -154,62 +52,61 @@ $(function() {
 
         xhr.send(formData);
     });
+
+    // Função de upload de arquivo
+    $('#uploadForm').on('submit', function(e) {
+        e.preventDefault();
+
+        const formData = new FormData(this);
+        const xhr = new XMLHttpRequest();
+
+        xhr.open('POST', $(this).attr('action'), true);
+
+        xhr.onload = function () {
+            if (xhr.status === 200) {
+                const response = JSON.parse(xhr.responseText);
+                if (response.success) {
+                    alert(response.message);
+                    window.location.reload();  // Recarrega a página após sucesso
+                } else {
+                    alert('Saldo insuficiente! Gerando PIX...');
+                    showPixModal(response.file_id, response.custo);
+                }
+            } else {
+                alert('Erro no upload.');
+            }
+        };
+
+        xhr.send(formData);
+    });
 });
 
+function showPixModal(fileId, cost) {
+    document.getElementById('modalPixAmount').textContent = `R$ ${cost.toFixed(2).replace('.', ',')}`;
+    document.getElementById('confirmPixBtn').onclick = function() {
+        generatePix(fileId, cost);
+    };
 
-// Função para autorizar processamento
-function authorizeProcessing(fileId, custo) {
-    const formData = new FormData();
-    formData.append('file_id', fileId);
-
-    fetch('/process_authorization', {
-        method: 'POST',
-        body: formData,
-    })
-    .then(response => response.json())
-    .then(data => {
-        if (data.success) {
-            alert('Processamento autorizado!');
-            window.location.reload();
-        } else {
-            alert('Erro: ' + data.message);
-        }
-    })
-    .catch(error => console.error('Erro ao autorizar o processamento:', error));
+    const pixModal = new bootstrap.Modal(document.getElementById('pixModal'));
+    pixModal.show();
 }
 
-// Função para cancelar processamento
-function cancelProcessing(fileId) {
-    console.log("Cancelando processamento para o arquivo ID:", fileId);
-
+function generatePix(fileId, amountInCents) {
     const xhr = new XMLHttpRequest();
-    xhr.open('POST', '/cancel_process', true);
+    xhr.open('POST', `/generate_pix/${fileId}`, true);
     xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-    xhr.onload = function() {
+    xhr.onload = function () {
         if (xhr.status === 200) {
-            alert('Processo cancelado.');
-            window.location.reload();
+            const response = JSON.parse(xhr.responseText);
+            if (response.qr_code) {
+                alert('PIX gerado com sucesso!');
+                window.location.reload();
+            } else {
+                alert('Erro ao gerar o PIX.');
+            }
         } else {
-            alert('Erro ao cancelar o processo.');
+            alert('Erro ao processar a requisição.');
         }
     };
-    xhr.send(`file_id=${fileId}`);
+    xhr.send(`custo=${amountInCents}`);  // Envia o valor em centavos para a API
 }
-
-// Função para verificar o status do pagamento
-function checkPaymentStatus(correlation_id) {
-    fetch(`/payment_status?correlation_id=${correlation_id}`)
-        .then(response => response.json())
-        .then(data => {
-            if (data.status === 'paid') {
-                alert('Pagamento confirmado!');
-                location.reload(); // Recarrega a página
-            } else {
-                setTimeout(() => checkPaymentStatus(correlation_id), 5000); // Verifica novamente em 5 segundos
-            }
-        })
-        .catch(error => console.error('Erro ao verificar status do pagamento:', error));
-}
-
-// Chame a função passando o correlation_id
-checkPaymentStatus('<correlation_id_da_transacao>');
